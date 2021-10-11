747dd78260e43de0d242279d3b125fe049505714
Tarun Kishore <tarun2kishore@gmail.com>
2021-10-11 17:56:20 +0530

    applied redirection

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,35 +3,38 @@ const router = express.Router();
 
 router.get("/login", (req, res) => {
   if (req.cookies.token) return res.redirect("/");
-
+  
   res.render("login");
 });
 
 router.get("/signup", (req, res) => {
   if (req.cookies.token) return res.redirect("/");
-
+  
   res.render("signup");
 });
 
 router.get("/success", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
-
+  
   res.render("success", options);
 });
 
-router.get("/", (req, res) => {
-  const options = {};
-  if (req.cookies.token) options.loggedIn = true;
-
-  res.render("index", options);
-});
 
 router.get("/about", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
-
+  
   res.render("about", options);
+});
+
+router.get("/", (req, res) => {
+  res.redirect('/login')
+  // const options = {};
+  // if (req.cookies.token) options.loggedIn = true;
+
+  
+  // res.render("index", options);
 });
 
 router.get("*", (req, res) => {
