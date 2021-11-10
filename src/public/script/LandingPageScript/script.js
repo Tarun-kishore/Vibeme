94c17bb492aed3554c3cb5be9d9ba0b22c00af45
Tarun Kishore <tarun2kishore@gmail.com>
2021-11-10 16:43:00 +0530

    fixed video loading issue

---
+++ src/public/script/LandingPageScript/script.js
@@ -0,0 +1,19 @@
+let loader = document.querySelector('#preloader');
+let text1 = document.querySelector('.to-be-animated-text1');
+let text2 = document.querySelector('.to-be-animated-text2');
+let para = document.querySelector('.to-be-animated-para');
+let anchor = document.querySelector('.to-be-animated-anchor');
+
+const $videoElement = document.querySelector('video')
+
+window.addEventListener('load', () => {
+    $videoElement.addEventListener('loadeddata',()=>{
+        if($videoElement.readyState >= 3){
+        loader.style.display = "none";
+        text1.classList.add('text1');
+        text2.classList.add('text2');
+        para.classList.add('animate__animated', 'animate__fadeInLeftBig');
+        anchor.classList.add('animate__animated', 'animate__fadeInRightBig');
+        }
+    })
+});
