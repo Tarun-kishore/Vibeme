8df8981cb6198fd3e5e7e6e8c1354283dc4b6dcf
Tarun Kishore <tarun2kishore@gmail.com>
2021-11-10 16:51:53 +0530

    merging

--- src/public/script/LandingPageScript/script.js
+++ src/public/script/LandingPageScript/script.js
@@ -7,13 +7,17 @@ let anchor = document.querySelector('.to-be-animated-anchor');
 const $videoElement = document.querySelector('video')
 
 window.addEventListener('load', () => {
+    console.log('checkpoint');
     $videoElement.addEventListener('loadeddata',()=>{
-        // if($videoElement.readyState >= 3){
+    console.log('checkpoint');
+        if($videoElement.readyState >= 3){
+    console.log('checkpoint');
         loader.style.display = "none";
         text1.classList.add('text1');
         text2.classList.add('text2');
         para.classList.add('animate__animated', 'animate__fadeInLeftBig');
         anchor.classList.add('animate__animated', 'animate__fadeInRightBig');
-        // }
+        console.log('checkpoint');
+        }
     })
 });
