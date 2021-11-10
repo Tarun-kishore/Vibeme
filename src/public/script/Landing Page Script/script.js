e96f0d0cb2cd0a476f0e9f6ebe53bfbc12c6be23
Legend-1409 <yashbansal1409@gmail.com>
2021-11-10 13:59:37 +0530

    changing links of pages

---
+++ src/public/script/Landing Page Script/script.js
@@ -0,0 +1,3 @@
+// $('video').on('loadeddata', () => {
+//     $('section').append('<div div class= "overlay" ></div > <div class="text"> <div class="animation-container"> <span class="text1">Lets Vibe With</span> <span class="text2">VibeMe</span> </div> <p class="animate__animated animate__bounceInLeft">become part of our community</p> <a class="animate__animated  animate__bounceInRight" href="#">Start Vibing</a> </div>');
+// });
