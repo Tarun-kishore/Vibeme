20da3ea19991d6d4c87d4fa5a8c555fe779370a9
Legend-1409 <yashbansal1409@gmail.com>
2021-11-15 22:09:37 +0530

    added comments for landing page code

--- src/public/script/LandingPageScript/script.js
+++ src/public/script/LandingPageScript/script.js
@@ -1,11 +1,12 @@
+// Selecting the loader, text1, text2, para and anchor
 let loader = document.querySelector('#preloader');
 let text1 = document.querySelector('.to-be-animated-text1');
 let text2 = document.querySelector('.to-be-animated-text2');
 let para = document.querySelector('.to-be-animated-para');
 let anchor = document.querySelector('.to-be-animated-anchor');
 
-const $videoElement = document.querySelector('video')
-
+// To make sure our animation starts only after our complete code has been rendered on user's browser screen
+// Till then our loader will be displayed to user
 window.addEventListener('load', () => {
     loader.style.display = "none";
     text1.classList.add('text1');
