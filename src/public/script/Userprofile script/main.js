abb8b7c52e5cffd9346efae76c866bd00c3b29a6
Legend-1409 <yashbansal1409@gmail.com>
2021-11-16 14:45:37 +0530

    fixed time for profile page bottom top arrow

--- src/public/script/Userprofile script/main.js
+++ src/public/script/Userprofile script/main.js
@@ -105,7 +105,7 @@
   $('.back-to-top').click(function() {
     $('html, body').animate({
       scrollTop: 0
-    }, 1500, 'easeInOutExpo');
+    }, 150, 'easeInOutExpo');
     return false;
   });
 
