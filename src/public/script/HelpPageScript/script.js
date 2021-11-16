424b505a6ceefc6bab711215c1a80e66f9d83d82
Legend-1409 <yashbansal1409@gmail.com>
2021-11-16 11:04:38 +0530

    added help page comments

--- src/public/script/HelpPageScript/script.js
+++ src/public/script/HelpPageScript/script.js
@@ -1,9 +1,12 @@
+// selected list text and their heading
 let accordion = document.querySelector('.accordion-list');
 let items = accordion.querySelectorAll('li');
 let questions = accordion.querySelectorAll('.accordion-title');
 
+// now on each li title, adding event listener to hear click
 questions.forEach(question => question.addEventListener('click', toggleAccordion));
 
+// now we get the li which is being clicked so we open it and remove others li 
 function toggleAccordion(){
     thisItem = this.parentNode;
     items.forEach(item => {
