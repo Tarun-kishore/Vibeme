35252eabd0c4507975174c2a1956cd9806015fce
Tarun Kishore <tarun2kishore@gmail.com>
2021-11-29 12:44:33 +0530

    fixed route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,60 +3,57 @@ const router = express.Router();
 
 router.get("/login", (req, res) => {
   if (req.cookies.token) return res.redirect("/user/profile");
-  
+
   res.render("UserActivity/login");
 });
 
 router.get("/signup", (req, res) => {
-  res.redirect('/login')
+  res.redirect("/login");
 });
 
 router.get("/success", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
-  
+
   res.render("IndexPages/success", options);
 });
-
 
 router.get("/about", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
-  
+
   res.render("IndexPages/about", options);
-})
+});
 
 router.get("/feature", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
-  
+
   res.render("IndexPages/feature", options);
-})
+});
 
 router.get("/help", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
-  
+
   res.render("IndexPages/help", options);
-})
+});
 
 router.get("/", (req, res) => {
- 
   // res.redirect('/login')
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
 
-  
   res.render("IndexPages/index", options);
 });
-router.get('/notAuth'm (req,res)=>{
-    res.render('IndexPages/notAuth')
-})
+router.get("/notAuth", (req, res) => {
+  res.render("IndexPages/notAuth");
+});
 
-router.get('/success'm (req,res)=>{
-    res.render('IndexPages/success')
-})
- 
+router.get("/success", (req, res) => {
+  res.render("IndexPages/success");
+});
+
 router.get("*", (req, res) => {
   const options = {};
   if (req.cookies.token) options.loggedIn = true;
